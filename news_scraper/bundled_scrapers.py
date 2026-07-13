444bd7a1ec3cdd598baa4978c076e07e95ca737a
ECJura2000 <ericchitpe@gmail.com>
2026-07-13 10:23:36 +0800

    bundle dynamically loaded scraper modules

---
+++ news_scraper/bundled_scrapers.py
@@ -0,0 +1,27 @@
+"""Static imports for PyInstaller's analysis of dynamically loaded scrapers."""
+
+from news_scraper.scrapers.ministry.agriculture import afa, afna, aphia, ardswc, atp, fa, moa
+from news_scraper.scrapers.ministry.communities import cip, hakka
+from news_scraper.scrapers.ministry.culture import moc, npm
+from news_scraper.scrapers.ministry.defense import indsr, mnd, ncsist
+from news_scraper.scrapers.ministry.development import ndc, niar, nstc, tasa
+from news_scraper.scrapers.ministry.digital import moda
+from news_scraper.scrapers.ministry.economy import moea
+from news_scraper.scrapers.ministry.education import moe, naer
+from news_scraper.scrapers.ministry.environment import moenv
+from news_scraper.scrapers.ministry.executive import ey
+from news_scraper.scrapers.ministry.finance import cbc, dgbas, fsc, ftc, mof
+from news_scraper.scrapers.ministry.foreign import mac, mofa, ocac
+from news_scraper.scrapers.ministry.health import cdc, fda, hpa, mohw, sfaa
+from news_scraper.scrapers.ministry.interior import moi, nfa, nlma, nlsc, npa, nps
+from news_scraper.scrapers.ministry.judicial import judicial_yuan
+from news_scraper.scrapers.ministry.justice import mjac, moj, tps
+from news_scraper.scrapers.ministry.labor import blf, mol, osha, wda
+from news_scraper.scrapers.ministry.oceans import cga, fleet, investigation, oac
+from news_scraper.scrapers.ministry.oversight import cy
+from news_scraper.scrapers.ministry.regulators import cec, ncc, pcc
+from news_scraper.scrapers.ministry.sports import sports
+from news_scraper.scrapers.ministry.transport import cwa, freeway, motc, motcmpb, thb, tourism
+from news_scraper.scrapers.ministry.veterans import vac, vghtpe
+
+__all__ = []
